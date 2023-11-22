c86d6bb444ae3134d2eb1c8e511182ba8649d180
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2023-11-22 20:12:09 +0000

    Go back to using marksman

--- lua/yutanagano/setup_plugins/lsp.lua
+++ lua/yutanagano/setup_plugins/lsp.lua
@@ -30,7 +30,7 @@ end)
 
 mason.setup({})
 mason_lspconfig.setup({
-  ensure_installed = { "pylsp", "rust_analyzer", "lua_ls", "texlab", "zk" },
+  ensure_installed = { "pylsp", "rust_analyzer", "lua_ls", "texlab", "marksman" },
   handlers = {
     lsp_zero.default_setup,
     
