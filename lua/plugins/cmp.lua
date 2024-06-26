a3428890a9ae37606e1ed3962c9cb9d63111421c
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-06-26 21:32:12 +0100

    Add nvim autopairs

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,4 +3,11 @@ return {
    "L3MON4D3/LuaSnip",
    "saadparwaiz1/cmp_luasnip",
    "hrsh7th/cmp-nvim-lsp",
+   {
+      'windwp/nvim-autopairs',
+      event = "InsertEnter",
+      config = true
+      -- use opts = {} for passing setup options
+      -- this is equalent to setup({}) function
+   },
 }
