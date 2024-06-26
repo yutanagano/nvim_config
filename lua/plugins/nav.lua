7fa3f76f76f1091e589fb99266cefe9e475af4bb
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-06-26 22:04:21 +0100

    Add buffer navigation shortcut

--- lua/plugins/nav.lua
+++ lua/plugins/nav.lua
@@ -5,11 +5,5 @@ return {
       dependencies = {
          "nvim-lua/plenary.nvim",
       },
-      config = function()
-	 local builtin = require('telescope.builtin')
-	 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-	 vim.keymap.set('n', '<leader>g', builtin.git_files, {})
-	 vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
-      end,
    },
 }
