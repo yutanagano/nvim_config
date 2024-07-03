751291d4757fa12bb4858dc2cf5132ff45c7f5c8
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-07-03 17:05:40 +0100

    Separate keymap customisation into its own file

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -9,5 +9,10 @@ vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 
 -- language and refactoring
 vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, {})
+
+-- yanking and pasting
 vim.keymap.set('', '<leader>y', '"+y', {})
 vim.keymap.set('n', '<leader>p', '"+p', {})
+
+-- spelling
+vim.keymap.set('n', '<leader>s', ':setlocal spell!<CR>', {})
--- lua/nvim_options.lua
+++ lua/nvim_options.lua
@@ -2,3 +2,5 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.shiftwidth = 3
+
+vim.opt.spelllang = {"en_gb"}
