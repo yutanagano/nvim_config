73bcbfdd1bbc3d9618e136d2fade2132dc55dde6
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2024-09-15 14:25:36 +0100

    Configure treesitter and enable highlighting by default

--- after/plugin/language.lua
+++ after/plugin/language.lua
@@ -1,3 +1,17 @@
+require("nvim-treesitter.configs").setup({
+   auto_install = true,
+   highlight = {
+      enable = true,
+      disable = function(_, buf)
+	   local max_filesize = 100 * 1024 -- 100 KB
+	   local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+	   if ok and stats and stats.size > max_filesize then
+	       return true
+	   end
+       end,
+   }
+})
+
 require("mason").setup()
 require("mason-lspconfig").setup({
    ensure_installed = { "pylsp", "texlab", "marksman", "lua_ls", "gopls" },
