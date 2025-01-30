6e400382af3c67bc5db978a7605be3a065ce1596
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-01-30 12:59:57 +0000

    set options for python indenting

--- lua/nvim_options.lua
+++ lua/nvim_options.lua
@@ -11,3 +11,9 @@ vim.opt.spelllang = {"en_gb"}
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+-- python
+vim.g.python_indent = {
+  open_paren = 'shiftwidth()',
+  closed_paren_align_last_line = false
+}
