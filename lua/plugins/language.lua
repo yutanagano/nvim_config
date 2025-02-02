0732f8960b5800ec469f7f9821f9569bd6bc517c
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-02-02 22:50:33 +0000

    remove conform lines for languages I don't care about now

--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -47,10 +47,6 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "black" },
-				-- You can customize some of the format options for the filetype (:help conform.format)
-				-- rust = { "rustfmt", lsp_format = "fallback" },
-				-- Conform will run the first available formatter
-				-- javascript = { "prettierd", "prettier", stop_after_first = true },
 			},
 			format_on_save = {
 				-- I recommend these options. See :help conform.format for details.
