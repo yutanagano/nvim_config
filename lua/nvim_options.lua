48bd5333331ee69206523c3041668cbe4d65f2e7
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-02-03 12:51:05 +0000

    refactor cmp plugin setup

--- lua/nvim_options.lua
+++ lua/nvim_options.lua
@@ -1,20 +1,25 @@
+-- ui
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.colorcolumn = "100"
 vim.opt.scrolloff = 5
 
+-- indent
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.tabstop = 2
-
-vim.opt.spelllang = { "en_gb" }
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- python
 vim.g.python_indent = {
 	open_paren = "shiftwidth()",
 	closed_paren_align_last_line = false,
 }
+
+-- spelling
+vim.opt.spelllang = { "en_gb" }
+
+-- fuzzy search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- bullets.nvim
+vim.g.bullets_outline_levels = { "num", "std-", "std*", "std+" }
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,16 +1,77 @@
 return {
 	{
 		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"L3MON4D3/LuaSnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-path",
+		},
 		opts = function(_, opts)
-			opts.sources = opts.sources or {}
-			table.insert(opts.sources, {
-				name = "lazydev",
-				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+
+			opts.snippet = {
+				-- REQUIRED - you must specify a snippet engine
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+					-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+				end,
+			}
+
+			opts.window = {
+				-- completion = cmp.config.window.bordered(),
+				-- documentation = cmp.config.window.bordered(),
+			}
+
+			opts.mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						if luasnip.expandable() then
+							luasnip.expand()
+						else
+							cmp.confirm({
+								select = true,
+							})
+						end
+					else
+						fallback()
+					end
+				end),
+
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.locally_jumpable(1) then
+						luasnip.jump(1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			})
+
+			opts.sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "path" },
+				{ name = "lazydev", group_index = 0 }, -- set group index to 0 to skip loading LuaLS completions
 			})
 		end,
 	},
-	"L3MON4D3/LuaSnip",
-	"saadparwaiz1/cmp_luasnip",
 	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
@@ -18,6 +79,5 @@ return {
 		-- use opts = {} for passing setup options
 		-- this is equalent to setup({}) function
 	},
-	"hrsh7th/cmp-path",
 	"bullets-vim/bullets.vim",
 }
