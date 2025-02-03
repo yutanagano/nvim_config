cb654878c32ff558a197118c89904542bbd89ae8
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-02-03 12:51:42 +0000

    refactor colorscheme setup

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,36 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-  }
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = function(_, opts)
+			opts.colors = {
+				theme = {
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
+					},
+				},
+			}
+			opts.overrides = function(colors)
+				local theme = colors.theme
+				return {
+					["@markup.list.markdown"] = { fg = theme.syn.special2 },
+					["@markup.heading.markdown"] = { fg = theme.syn.identifier, bold = true },
+					["@markup.heading.2.markdown"] = { fg = theme.syn.identifier, bold = true },
+					["@markup.heading.3.markdown"] = { fg = theme.syn.string, bold = true },
+					["@markup.heading.4.markdown"] = { fg = theme.syn.type, bold = true },
+					-- ["@lsp.type.class.markdown"] = { fg = theme.syn.fun },
+					LineNr = { fg = theme.syn.constant },
+					LineNrAbove = { fg = theme.ui.nontext },
+					LineNrBelow = { fg = theme.ui.nontext },
+				}
+			end
+		end,
+		config = function(_, opts)
+			require("kanagawa").setup(opts)
+			vim.cmd("colorscheme kanagawa")
+		end,
+	},
 }
