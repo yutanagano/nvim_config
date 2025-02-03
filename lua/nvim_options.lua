d1114047b00e23f6c05f9ff6ebc1278b19a3a90b
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-02-03 17:14:12 +0000

    return to four spaces for tabs in markdown

--- lua/nvim_options.lua
+++ lua/nvim_options.lua
@@ -13,7 +13,7 @@ vim.g.python_indent = {
 	open_paren = "shiftwidth()",
 	closed_paren_align_last_line = false,
 }
-vim.g.markdown_recommended_style = false
+-- vim.g.markdown_recommended_style = false
 
 -- spelling
 vim.opt.spelllang = { "en" }
