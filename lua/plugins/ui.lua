e0c9c26a42271a9ee65a05163b7021e59570068b
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-02-04 17:29:35 +0000

    add telescope-ui-select

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,9 +1,21 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    branch = "0.1.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
+		opts = {
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_cursor(),
+				},
+			},
+		},
+		config = function(_, opts)
+			require("telescope").setup(opts)
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
