58c623911b146960f5e536b0f16966c4eb7f9d5b
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-02-04 23:48:22 +0000

    add dark / light mode toggle

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -5,7 +5,6 @@ vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Find files" })
 vim.keymap.set("n", "<leader>g", builtin.git_files, { desc = "Find git files" })
 vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Grep workspace" })
 vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Find buffers" })
-vim.keymap.set("n", "<leader>d", builtin.diagnostics, { desc = "List diagnostics" })
 vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "Go to definition(s)" })
 vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "Go to reference(s)" })
 vim.keymap.set("n", "gl", ":bnext<CR>", { desc = "Go to next buffer" })
@@ -19,6 +18,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- language and refactoring
+vim.keymap.set("n", "<leader>d", builtin.diagnostics, { desc = "List diagnostics" })
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename symbol" })
 vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, { desc = "Code action" })
 
@@ -28,6 +28,18 @@ vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from clipboard" })
 
 -- spelling
 vim.keymap.set("n", "<leader>s", ":setlocal spell!<CR>", { desc = "Toggle spelling" })
+
+-- ui
+vim.keymap.set("n", "<leader>c", function()
+	local current_bg = vim.o.background or "dark"
+
+	if current_bg == "dark" then
+		vim.o.background = "light"
+		return
+	end
+
+	vim.o.background = "dark"
+end, { desc = "Toggle dark / light mode" })
 
 -- markdown table alignment (cursor must be inside table, table must be isolated paragraph)
 vim.keymap.set(
