627e0e07de6645add4165c72790c43d034e4dbd4
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-03-25 11:00:30 +0000

    disable nvim cmp preselect

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -25,35 +25,37 @@ return {
 				-- documentation = cmp.config.window.bordered(),
 			}
 
+			opts.preselect = cmp.PreselectMode.None
+
 			opts.mapping = cmp.mapping.preset.insert({
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
-				-- ["<CR>"] = cmp.mapping(function(fallback)
-				-- 	if cmp.visible() then
-				-- 		if luasnip.expandable() then
-				-- 			luasnip.expand()
-				-- 		else
-				-- 			cmp.confirm({
-				-- 				select = true,
-				-- 			})
-				-- 		end
-				-- 	else
-				-- 		fallback()
-				-- 	end
-				-- end),
-				["<CR>"] = cmp.mapping({
-					i = function(fallback)
-						if cmp.visible() and cmp.get_active_entry() then
-							cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+				["<CR>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						if luasnip.expandable() then
+							luasnip.expand()
 						else
-							fallback()
+							cmp.confirm({
+								select = true,
+							})
 						end
-					end,
-					s = cmp.mapping.confirm({ select = true }),
-					c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-				}),
+					else
+						fallback()
+					end
+				end),
+				-- ["<CR>"] = cmp.mapping({
+				-- 	i = function(fallback)
+				-- 		if cmp.visible() and cmp.get_active_entry() then
+				-- 			cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+				-- 		else
+				-- 			fallback()
+				-- 		end
+				-- 	end,
+				-- 	s = cmp.mapping.confirm({ select = true }),
+				-- 	c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+				-- }),
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_next_item()
