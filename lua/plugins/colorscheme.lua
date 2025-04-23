df57d5cae65b6e15ed1f186d5b35b2d942bc8ed1
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-04-23 16:47:55 +0100

    update colorscheme to have prettier bordered floating windows

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,8 +4,24 @@ return {
 		lazy = false,
 		priority = 1000,
 		opts = function(_, opts)
+			local dragon_colors = require("kanagawa.colors").setup({ theme = "dragon" })
 			opts.colors = {
 				theme = {
+					dragon = {
+						ui = {
+							fg = dragon_colors.palette.fujiWhite,
+							float = {
+								fg = dragon_colors.palette.fujiWhite,
+								bg = dragon_colors.theme.ui.bg,
+								bg_border = dragon_colors.theme.ui.bg,
+							},
+							pmenu = {
+								fg = dragon_colors.palette.fujiWhite,
+								bg = dragon_colors.theme.ui.bg,
+								bg_sel = dragon_colors.theme.ui.bg_p2,
+							},
+						},
+					},
 					all = {
 						ui = {
 							bg_gutter = "none",
@@ -22,8 +38,7 @@ return {
 					["@markup.heading.3.markdown"] = { fg = theme.syn.string, bold = true },
 					["@markup.heading.4.markdown"] = { fg = theme.syn.type, bold = true },
 					["@lsp.type.decorator.markdown"] = { fg = theme.syn.special1 },
-					LineNr = { fg = theme.ui.nontext },
-					CursorLineNr = { fg = theme.syn.constant },
+					-- BlinkCmpMenuBorder = { link = "FloatBorder" },
 				}
 			end
 		end,
