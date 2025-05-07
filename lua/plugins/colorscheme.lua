a20aebcee59a038a0ea1f5bb968da0b70e4fc3f7
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-05-07 18:57:10 +0100

    revert to vanilla kanagawa dragon colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,10 @@
 return {
+	-- {
+	-- 	"vague2k/vague.nvim",
+	-- 	config = function()
+	-- 		vim.cmd("colorscheme vague")
+	-- 	end,
+	-- },
 	{
 		"rebelot/kanagawa.nvim",
 		lazy = false,
@@ -29,18 +35,18 @@ return {
 					},
 				},
 			}
-			opts.overrides = function(colors)
-				local theme = colors.theme
-				return {
-					["@markup.list.markdown"] = { fg = theme.syn.special2 },
-					["@markup.heading.markdown"] = { fg = theme.syn.identifier, bold = true },
-					["@markup.heading.2.markdown"] = { fg = theme.syn.identifier, bold = true },
-					["@markup.heading.3.markdown"] = { fg = theme.syn.string, bold = true },
-					["@markup.heading.4.markdown"] = { fg = theme.syn.type, bold = true },
-					["@lsp.type.decorator.markdown"] = { fg = theme.syn.special1 },
-					-- BlinkCmpMenuBorder = { link = "FloatBorder" },
-				}
-			end
+			-- opts.overrides = function(colors)
+			-- 	local theme = colors.theme
+			-- 	return {
+			-- 		["@markup.list.markdown"] = { fg = theme.syn.special2 },
+			-- 		["@markup.heading.markdown"] = { fg = theme.syn.identifier, bold = true },
+			-- 		["@markup.heading.2.markdown"] = { fg = theme.syn.identifier, bold = true },
+			-- 		["@markup.heading.3.markdown"] = { fg = theme.syn.string, bold = true },
+			-- 		["@markup.heading.4.markdown"] = { fg = theme.syn.type, bold = true },
+			-- 		["@lsp.type.decorator.markdown"] = { fg = theme.syn.special1 },
+			-- 		-- BlinkCmpMenuBorder = { link = "FloatBorder" },
+			-- 	}
+			-- end
 		end,
 		config = function(_, opts)
 			require("kanagawa").setup(opts)
