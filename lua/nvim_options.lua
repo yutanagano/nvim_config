72d2eb03628d1b4dcd0588138fb6db0b08e5da37
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-05-20 23:20:37 +0100

    feat: add yank highlighting option

--- lua/nvim_options.lua
+++ lua/nvim_options.lua
@@ -4,6 +4,13 @@ vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
 vim.diagnostic.config({ float = { border = "rounded" } })
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Hightlight selection on yank",
+	callback = function()
+		vim.hl.on_yank({ timeout = 100 })
+	end,
+})
+--
 -- indent
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
@@ -18,6 +25,7 @@ vim.g.markdown_recommended_style = false
 -- spelling
 vim.opt.spelllang = { "en" }
 vim.api.nvim_create_autocmd("FileType", {
+	desc = "Turn on spell check for prose files",
 	pattern = { "markdown", "latex" },
 	callback = function()
 		vim.opt_local.spell = true
