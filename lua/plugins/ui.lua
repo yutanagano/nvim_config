b41d6df09b37b286fc6f673d736c763f969a72b0
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-08-08 16:56:43 +0900

    remove todo-comments

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,14 +1,5 @@
 return {
 	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {
-			highlight = {
-				comments_only = false,
-			},
-		},
-	},
-	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
 		---@module 'render-markdown'
