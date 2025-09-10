27c6dc6a7001b8d3ae287a1b494123c1c4dc81dd
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-09-10 10:20:37 +0100

    Revert "revert back to telescope"
    This reverts commit cf90b619054396fb5a1c1353ff93f0c14497a6a1.

--- lua/plugins/nav.lua
+++ lua/plugins/nav.lua
@@ -1,67 +1,29 @@
 return {
 	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope-ui-select.nvim",
-		},
-		config = function(_, opts)
-			opts.extensions = opts.extensions or {}
-			opts.extensions["ui-select"] = {
-				require("telescope.themes").get_cursor(),
-			}
-			require("telescope").setup(opts)
-			require("telescope").load_extension("ui-select")
-		end,
+		"nvim-mini/mini.pick",
+		enabled = true,
+		dependencies = "nvim-mini/mini.icons",
 		keys = {
 			{
 				"<leader>f",
 				function()
-					require("telescope.builtin").find_files()
+					require("mini.pick").builtin.files()
 				end,
 				desc = "Find files",
 			},
 			{
-				"<leader>g",
-				function()
-					require("telescope.builtin").git_files()
-				end,
-				desc = "Find git files",
-			},
-			{
 				"<leader>/",
 				function()
-					require("telescope.builtin").live_grep()
+					require("mini.pick").builtin.grep_live()
 				end,
 				desc = "Grep workspace",
 			},
 			{
 				"<leader>b",
 				function()
-					require("telescope.builtin").buffers()
+					require("mini.pick").builtin.buffers()
 				end,
 				desc = "Find buffers",
-			},
-			{
-				"<leader>d",
-				function()
-					require("telescope.builtin").diagnostics()
-				end,
-				desc = "List diagnostics",
-			},
-			{
-				"gd",
-				function()
-					require("telescope.builtin").lsp_definitions()
-				end,
-				desc = "Go to definition(s)",
-			},
-			{
-				"gr",
-				function()
-					require("telescope.builtin").lsp_references()
-				end,
-				desc = "Go to reference(s)",
 			},
 		},
 	},
