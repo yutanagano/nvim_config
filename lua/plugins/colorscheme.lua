c1eee3da1b29fc9dcaa193c682b66ad5e02c2f00
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-10-25 14:48:10 +0100

    switch to kanso colourscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,35 +1,28 @@
 return {
 	{
-		"rebelot/kanagawa.nvim",
+		"webhooked/kanso.nvim",
 		lazy = false,
 		priority = 1000,
-		opts = function(_, opts)
-			local dragon_colors = require("kanagawa.colors").setup({ theme = "dragon" })
-			opts.colors = {
-				theme = {
-					dragon = {
-						ui = {
-							float = {
-								bg = dragon_colors.theme.ui.bg,
-								bg_border = dragon_colors.theme.ui.bg,
-							},
-							pmenu = {
-								bg = dragon_colors.theme.ui.bg,
-								bg_sel = dragon_colors.theme.ui.bg_p2,
-							},
-						},
-					},
-					all = {
-						ui = {
-							bg_gutter = "none",
-						},
-					},
-				},
-			}
-		end,
 		config = function(_, opts)
-			require("kanagawa").setup(opts)
-			vim.cmd("colorscheme kanagawa-dragon")
+			opts = opts or {}
+			opts.background = {
+				dark = "zen",
+				light = "pearl",
+			}
+			require("kanso").setup(opts)
+			vim.cmd("colorscheme kanso")
 		end,
+		keys = {
+			{
+				"<leader>c",
+				function()
+					if vim.o.background == "light" then
+						vim.opt.background = "dark"
+					else
+						vim.opt.background = "light"
+					end
+				end,
+			},
+		},
 	},
 }
