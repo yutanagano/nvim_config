20b2cb4477fbec478c2252c90b04b7f0916c7e29
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2025-12-20 21:59:26 +0000

    Revert "update(color): switch to calcium"
    This reverts commit cc20930ac898af5b08b235cf013446d0357b0945.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,35 @@
 return {
 	{
-		"yutanagano/calcium.nvim",
-		config = function()
-			vim.cmd("colorscheme calcium")
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = function(_, opts)
+			local dragon_colors = require("kanagawa.colors").setup({ theme = "dragon" })
+			opts.colors = {
+				theme = {
+					dragon = {
+						ui = {
+							float = {
+								bg = dragon_colors.theme.ui.bg,
+								bg_border = dragon_colors.theme.ui.bg,
+							},
+							pmenu = {
+								bg = dragon_colors.theme.ui.bg,
+								bg_sel = dragon_colors.theme.ui.bg_p2,
+							},
+						},
+					},
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
+					},
+				},
+			}
+		end,
+		config = function(_, opts)
+			require("kanagawa").setup(opts)
+			vim.cmd("colorscheme kanagawa-dragon")
 		end,
 	},
 }
