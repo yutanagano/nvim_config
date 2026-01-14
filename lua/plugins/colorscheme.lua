b9942ae21314384f407b157c82f741e13937958f
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2026-01-14 10:11:42 +0000

    use lush-backed zenbones

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
 	{
 		"zenbones-theme/zenbones.nvim",
+		dependencies = "rktjmp/lush.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.g.zenbones_compat = 1
 			vim.cmd.colorscheme("zenbones")
 		end,
 	},
